376308d02dd5cfc65d0b7bb0a61e2b8428f6079e
Abnercuellar <abner2120@hotmail.com>
2022-02-14 17:31:11 -0500

    se crea base de datos

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ class Server{
         this.app.use(cors());
 
         this.app.use(express.json());
-        this.app.use(express.urlencoded({extended: false});
+        this.app.use(express.urlencoded({extended: false}));
     }
 
     start(): void {
